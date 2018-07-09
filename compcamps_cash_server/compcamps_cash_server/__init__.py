5e120b61e79fe812427d748911b35f57c7e757d5
Taylor Petrychyn <tpetrychyn@gmail.com>
2018-07-09 00:19:44 -0600

    point to new mongo

--- compcamps_cash_server/compcamps_cash_server/__init__.py
+++ compcamps_cash_server/compcamps_cash_server/__init__.py
@@ -11,7 +11,7 @@ client = MongoClient(os.getenv("MONGO_URL"),
                       password=os.getenv("MONGO_PASSWORD"),
                       authSource=os.getenv("MONGO_AUTHSOURCE"),
                       authMechanism='SCRAM-SHA-1')
-db = client.campcoin
+db = client.heroku_1q0qc73n
 
 template_dir = os.path.abspath('./views')
 app = Flask(__name__, template_folder='views', static_url_path=template_dir)
