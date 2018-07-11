16c38835c5fa1c803e1e9a2e0d6e708b22204aaa
Taylor Petrychyn <tpetrychyn@gmail.com>
2018-07-11 13:03:08 -0600

    step-5

--- compcamps_cash_miner/miner.py
+++ compcamps_cash_miner/miner.py
@@ -1,13 +1,19 @@
-from compcamps_cash_api.entities import Block, Transaction, Prefix
+from compcamps_cash_api.entities import Block, Transaction, Prefix, Keys
 from compcamps_cash_api import CompCampsCashApi
 
 ccapi = CompCampsCashApi("https://compcamps-cash.herokuapp.com")
 
 prefix = ccapi.getPrefix()
+# Calls the keys class from the ccapi
+myKeys = Keys()
+public_key, _ = myKeys.getEncodedKeys()
 
 transactions = ccapi.getPendingTransactions() # Gets the current transactions from the server
-# TODO: create a new transaction
-# TODO: Add your transaction to the transactions array
+
+# TODO: Replace your key string with the variable public_key
+transaction = Transaction("MINER", "PTPfVMu//rJr9SEeBXdr45cB/cdLYeoj6eMt93PqzkhzjUZz/4Et/o6xy0jg7WvdOvxEB78J81QG3VM0htWyzw==", 1)
+
+#TODO: Sign your transaction with your keys
 
 nonce = 0
 previousBlock = ccapi.getCurrentBlock()
@@ -18,7 +24,10 @@ while not block.validate(prefix):
     nonce = nonce + 1
     block = Block(previousBlock.index + 1, transactions, nonce, previousBlock.hash)
 
-if block.hash[0] != "0":
+res = ccapi.postBlock(block)
+print(res)
+
+if not block.validate(prefix):
     print("Invalid Hash.")
 else:
     print("Correct hash!")
