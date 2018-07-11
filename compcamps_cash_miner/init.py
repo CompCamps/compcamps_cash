082e7f3f6cb9dc635410466efaa460235eba2a10
Taylor Petrychyn <tpetrychyn@gmail.com>
2018-07-11 12:25:47 -0600

    step-1

--- compcamps_cash_miner/init.py
+++
@@ -1,7 +0,0 @@
-from compcamps_cash_api import CompCampsCashApi
-from compcamps_cash_api.entities import Keys
-
-# Initialize public/private key library
-myKeys = Keys()
-public_key, _ = myKeys.getEncodedKeys()
-ccapi = CompCampsCashApi("https://compcamps-cash.herokuapp.com") # Initalize & establish connection to CCC Server
--- compcamps_cash_miner/miner.py
+++ compcamps_cash_miner/miner.py
@@ -1,52 +1,12 @@
-import datetime as date
-import random
+import hashlib
 
-from compcamps_cash_api.entities import Block, Transaction, Keys, Prefix
-from compcamps_cash_api.entities.block import nextBlock
-from compcamps_cash_api import CompCampsCashApi
+data = "hello world"
 
-from init import myKeys, public_key, ccapi
+hash = hashlib.sha256(data.encode('utf-8')).hexdigest()
 
-def getLatestBlock():
-    return ccapi.getCurrentBlock()
+# TODO: Use a while loop here to change data and rehash until it starts with 0
 
-def getLatestTransactions():
-    #Get transactions waiting to be mined
-    transactions = ccapi.getPendingTransactions()
-
-    # Add a transaction paying yourself a coin
-    transaction = Transaction("MINER", public_key, 1)
-    transaction.sign(myKeys) # Sign the transaction with your keys
-    transactions.append(transaction)
-
-    return transactions
-
-def mine():
-    # Get latest block & transactions to mine
-    previousBlock = getLatestBlock()
-    transactions = getLatestTransactions()
-    prefix = ccapi.getPrefix()
-
-    # Some debug info
-    print("Current prefix: " + prefix)
-    print("Currently mining block: ")
-    previousBlock.display()
-    
-    # Mine the block by incrementing the nonce
-    nonce = 0
-    block = nextBlock(previousBlock, transactions, nonce)
-    beginTimestamp = date.datetime.now()
-    while not block.validate(prefix):
-        nonce = random.randint(1, 100000000000)
-        block = nextBlock(previousBlock, transactions, nonce)
-
-        # Check the server every 5 seconds incase someone else mined before us
-        if ((date.datetime.now() - beginTimestamp).total_seconds() > 5):
-            beginTimestamp = date.datetime.now()
-            previousBlock = getLatestBlock()
-            transactions = getLatestTransactions()
-            prefix = ccapi.getPrefix()
-
-    res = ccapi.postBlock(block) # Submit the block to the server
-    if res:
-        print("Block successfully mined!")
+if hash[0] != "0":
+    print("Invalid Hash.")
+else:
+    print("Correct hash!")
