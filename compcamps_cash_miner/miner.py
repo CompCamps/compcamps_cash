8e2d3004079c6bf13fe69fdbbd18b4ae5682b42e
Taylor Petrychyn <tpetrychyn@gmail.com>
2018-07-11 12:55:39 -0600

    step-3

--- compcamps_cash_miner/miner.py
+++ compcamps_cash_miner/miner.py
@@ -1,18 +1,20 @@
-import hashlib
-from block import Block
+from compcamp_cash_api.entities import Block
+from compcamps_cash_api import CompCampsCashApi
+
+ccapi = CompCampsCashApi("https://compcamps-cash.herokuapp.com")
 
 data = "hello world"
 nonce = 0
-# Creates a genesis block
-block = Block(0, data, nonce, 0)
+# TODO: Instead of a blank block, use the ccapi to getCurrentBlock() from the server
+previousBlock = 
 
-# TODO: Move this into the Block class
-hash = hashlib.sha256(data.encode('utf-8')).hexdigest()
+# TODO: create the next block using the previousBlock values
+block = 
 
 # TODO: Change our loop to use our block's validate function
-while hash[0] != "0":
+while not block.validate("0"):
     nonce = nonce + 1
-    block = Block(0, data, nonce, 0)
+    block = # This line should match line 13
 
 if block.hash[0] != "0":
     print("Invalid Hash.")
